b1dbc1a436ed6b4ed52c67a3875bcdf7341ff5c9
gustavovalle23 <gustavo.vallerp@hotmail.com>
2020-09-15 23:04:31 -0300

    Intervalo

--- URI/Python/intervalo.py
+++ URI/Python/intervalo.py
@@ -1,12 +1,12 @@
 def intervalo(num):
     if num >= 0 and num <= 25:
-        return ('Intervalo [0, 25]')
+        return ('Intervalo [0,25]')
     elif num > 25 and num <= 50:
         return ('Intervalo (25,50]')
     elif num > 50 and num <= 75:
-        return ('Intervalo (50, 75]')
+        return ('Intervalo (50,75]')
     elif num > 75 and num <= 100:
-        return ('Intervalo (75, 100]')
+        return ('Intervalo (75,100]')
     else:
         return ('Fora de intervalo')
 
